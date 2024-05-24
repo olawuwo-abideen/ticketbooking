6eed19aed028558c0ea71a46fbae1fee723e8f5f
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-05-24 10:04:56 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bookingticket
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+
+import "fmt"
+
+
+func main()  {
+	fmt.Println("Welcome to our ticket booking application")
+	fmt.Println("Get your ticket here!")
+}
+
