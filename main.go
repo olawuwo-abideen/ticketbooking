d2909e37800db054a3a0c93986087f40d2a1d351
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-05-25 11:28:21 +0100

    update

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
-
 import "fmt"
 
+func main() {
 
-func main()  {
-	fmt.Println("Welcome to our ticket booking application")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 200
+	var remainingTickets uint = 100
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v ticket and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here!")
-}
 
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email %v \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
