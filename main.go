45283d76946c6d9f0fd5f623d6f1ba097aeb7421
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-05-26 02:23:31 +0100

    for loop

--- main.go
+++ main.go
@@ -13,31 +13,32 @@ func main() {
 	fmt.Printf("We have total of %v ticket and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here!")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
 
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("The whole Slice: %v\n", bookings)
-	fmt.Printf("The whole value: %v\n", bookings[0])
-	fmt.Printf("Slice type: %T\n", bookings)
-	fmt.Printf("Slice length: %v\n", len(bookings))
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email %v \n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email %v \n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
+
 }
